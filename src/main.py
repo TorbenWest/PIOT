18137dc1ad116cf146d8b6fce99fd602fb5f5fa6
Torben Westphalen <446501@student.fontys.nl>
2022-11-07 15:33:19 +0100

    Clean up

--- src/main.py
+++ src/main.py
@@ -14,22 +14,23 @@ async def main():
 
     # db_service.get_commands_for_bd_addresses(['A8:AB:B5:DC:DC:BF'])
     # db_service.insert_user(("torben", "password123", "A8:AB:B5:DC:DC:BF"), ("open", "close", "lock", "unlock"))
+    # db_service.insert_user(("Jord", "password123", "C0:DC:DA:96:FF:D1"), ("open", "close", "lock", "unlock"))
 
-    id = db_service.get_user('torben', 'password123')
-    print(id)
-    # bt_service = BluetoothService(db_service)
-    # m_service = MicrophoneService(bt_service)
-    # p = Periodic(lambda: bt_service.scan(config_service.bluetooth_config.get('discover_duration')),
-    #              config_service.bluetooth_config.get('scan_interval'))
-    #
-    # try:
-    #     await p.start()
-    #     await asyncio.sleep(30)
-    #     m_service.match_word('close')
-    #     await p.stop()
-    #     connector.close_connection()
-    # finally:
-    #     await p.stop()
+    # id = db_service.get_user('torben', 'password123')
+    # print(id)
+    bt_service = BluetoothService(db_service)
+    m_service = MicrophoneService(bt_service)
+    p = Periodic(lambda: bt_service.scan(config_service.bluetooth_config.get('discover_duration')),
+                 config_service.bluetooth_config.get('scan_interval'))
+
+    try:
+        await p.start()
+        await asyncio.sleep(30)
+        m_service.match_word('close')
+        await p.stop()
+        connector.close_connection()
+    finally:
+        await p.stop()
 
 
 if __name__ == '__main__':
