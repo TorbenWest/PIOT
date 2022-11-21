cb852bc930b17cd864bff367dfb77c525fa829c5
Torben Westphalen <446501@student.fontys.nl>
2022-11-21 16:21:09 +0100

    landing page

--- src/ui/main_ui.py
+++ src/ui/main_ui.py
@@ -1,10 +1,29 @@
 import PySimpleGUI as sg
+from PySimpleGUI import Button
 
-_layout: list[list] = [
-    [sg.Text('The Smart Door', justification='top')],
-    [sg.Button('OK')]
+
+def get_button(text: str) -> Button:
+    return sg.Button(text, size=(10, 1), border_width=4, font=("Arial", 20), key='-%s-' % text.upper())
+
+
+# Horizontal layout
+_layout = [
+    [get_button('Register'), get_button('Delete')],
+    [get_button('Settings'), get_button('Activate')],
+    [get_button('Users'), get_button('Deactivate')]
 ]
 
 
 def start_gui() -> None:
-    sg.Window(title='The Smart Door', layout=_layout, margins=(360, 250)).read()
+    # Create the window
+    window = sg.Window(title='The Smart Door', layout=_layout)
+    # Create an event loop
+    while True:
+        event, values = window.read()
+
+        if event == "OK" or event == sg.WIN_CLOSED:
+            break
+
+        if event == '-REGISTER-':
+            print('Register')
+    window.close()
