c60621a378d43ca558d05ad943b438234beb0549
Torben Westphalen <446501@student.fontys.nl>
2023-01-04 23:15:42 +0100

    Working on threading

--- src/main.py
+++ src/main.py
@@ -1,29 +1,31 @@
 import asyncio
-import sys
 import getopt
+import sys
 
+from periodic import Periodic
 from services.bluetooth_service import BluetoothService
 from services.config_service import ConfigService
 from services.database_service import MySqlConnector, DatabaseService
 from services.microphone_service import MicrophoneService
-from periodic import Periodic
 from ui.app import App
-from ui.main_ui import UIService
 
 
-# db_service.get_commands_for_bd_addresses(['A8:AB:B5:DC:DC:BF'])
-# db_service.insert_user(("torben", "password123", "A8:AB:B5:DC:DC:BF"), ("open", "close", "lock", "unlock"))
-# db_service.insert_user(("Jord", "password123", "C0:DC:DA:96:FF:D1"), ("open", "close", "lock", "unlock"))
-# id = db_service.get_user('torben', 'password123')
-# print(id)
-async def main(argv: list) -> None:
-    try:
-        opts, args = getopt.getopt(argv, "hbf", ["backend", "frontend"])
-    except getopt.GetoptError:
-        print('main.py -b')
-        print('main.py -f')
-        sys.exit(2)
+def frontend(event_loop):
+    config_service = ConfigService()
+    connector = MySqlConnector(config_service.database_config)
+    db_service = DatabaseService(connector.con)
+    bt_service = BluetoothService(db_service)
+    bluetooth_periodic = Periodic(lambda: bt_service.scan(config_service.bluetooth_config.get('discover_duration')),
+                                  config_service.bluetooth_config.get('scan_interval'))
 
+    # await bluetooth_periodic.start()
+    # await asyncio.sleep(30)
+    app = App(event_loop, bluetooth_periodic, db_service, connector)
+    # async_mainloop(app)
+    app.mainloop()
+
+
+async def backend():
     config_service = ConfigService()
     connector = MySqlConnector(config_service.database_config)
     db_service = DatabaseService(connector.con)
@@ -33,41 +35,7 @@ async def main(argv: list) -> None:
     bluetooth_periodic = Periodic(lambda: bt_service.scan(config_service.bluetooth_config.get('discover_duration')),
                                   config_service.bluetooth_config.get('scan_interval'))
 
-    microphone = None
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('main.py -b')
-            print('main.py -f')
-            sys.exit(2)
-        elif opt in ("-b", "--backend"):
-            microphone = Periodic(lambda: m_service.listen(), 0.1)
-        elif opt in ("-f", "--frontend"):
-            await bluetooth_periodic.start()
-            await asyncio.sleep(30)
-            # app = App(bluetooth_periodic, db_service, connector)
-            # app.mainloop()
-
-            # # TODO Let bluetooth service depend on ui service
-            # ui_service = UIService(db_service, bt_service)
-            # ui_periodic = Periodic(lambda: ui_service.start(), 10000)
-            # try:
-            #     await bluetooth_periodic.start()
-            #     await ui_periodic.start()
-            #     await asyncio.sleep(30)
-            #     await ui_periodic.stop()
-            #     await bluetooth_periodic.stop()
-            #     connector.close_connection()
-            # finally:
-            #     await ui_periodic.stop()
-            #     await bluetooth_periodic.stop()
-            #     connector.close_connection()
-            # sys.exit(2)
-
-    if microphone is None:
-        print('main.py -b')
-        print('main.py -f')
-        sys.exit(2)
+    microphone = Periodic(lambda: m_service.listen(), 0.1)
 
     try:
         await bluetooth_periodic.start()
@@ -85,5 +53,24 @@ async def main(argv: list) -> None:
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(sys.argv[1:]))
+    argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv, "hbf", ["backend", "frontend"])
+    except getopt.GetoptError:
+        print('main.py -b')
+        print('main.py -f')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -b')
+            print('main.py -f')
+            sys.exit(2)
+        elif opt in ("-b", "--backend"):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(backend())
+        elif opt in ("-f", "--frontend"):
+            loop = asyncio.get_event_loop()
+            # loop.run_until_complete(frontend(loop))
+            frontend(loop)
--- src/ui/app.py
+++ src/ui/app.py
@@ -1,4 +1,6 @@
 import customtkinter
+import asyncio
+import threading
 
 from database_service import DatabaseService, MySqlConnector
 from periodic import Periodic
@@ -14,12 +16,15 @@ customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard),
 # https://github.com/TomSchimansky/CustomTkinter
 # https://www.youtube.com/watch?v=vEfrIpj2NNw
 class App(customtkinter.CTk):
-    def __init__(self, bluetooth_periodic: Periodic, db_service: DatabaseService, connector: MySqlConnector) -> None:
+    def __init__(self, event_loop, bluetooth_periodic: Periodic, db_service: DatabaseService, connector: MySqlConnector) -> None:
         super().__init__()
+        self.async_loop = event_loop
         self.bluetooth_periodic = bluetooth_periodic
         self.connector = connector
         self.controller = UIController(db_service)
 
+        # async_handler(_run_async, lambda: self._start())
+        # self._run_task(self._start)
         app_width = 700
         app_height = 500
 
@@ -42,16 +47,11 @@ class App(customtkinter.CTk):
         self._create_button('Activate', 0.52, 0.375, lambda: Windows.login(self, Windows.activate))
         self._create_button('Users', 0.04, 0.7, lambda: Windows.users(self))
         self._create_button('Deactivate', 0.52, 0.7, lambda: Windows.login(self, Windows.deactivate))
-        self.protocol('WM_DELETE_WINDOW', lambda: self.on_exit())
+        self.protocol('WM_DELETE_WINDOW', lambda: self._run_task(self._stop()))
 
     def change_scaling_event(self, new_scaling: str) -> None:
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
-
-    async def on_exit(self) -> None:
-        await self.bluetooth_periodic.stop()
-        self.connector.close_connection()
-        self.destroy()
 
     def _create_button(self, text: str, relx: float, rely: float, command) -> None:
         button = customtkinter.CTkButton(self.main_frame, text=text,
@@ -60,3 +60,21 @@ class App(customtkinter.CTk):
                                          font=('Roboto', 50),
                                          command=command)
         button.place(relx=relx, rely=rely, relwidth=0.44, relheight=0.25)
+
+    """Stopping Bluetooth Service"""
+    def _run_task(self, func) -> None:
+        threading.Thread(target=self._asyncio_thread, args=(func,)).start()
+        self.destroy()
+
+    def _asyncio_thread(self, func):
+        self.async_loop.run_until_complete(func)
+
+    async def _stop(self) -> None:
+        await self.bluetooth_periodic.stop()
+        self.connector.close_connection()
+
+    async def _start(self) -> None:
+        await self.bluetooth_periodic.start()
+        await asyncio.sleep(30)
+        await self.bluetooth_periodic.stop()
+        self.connector.close_connection()
